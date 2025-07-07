97501e3841a69536ec66a7b38466be77049bdfdc
snaens <snaens@snaens.net>
2025-07-08 01:53:20 +0200

    improve launchfile

--- launch/teleop.launch.py
+++ launch/teleop.launch.py
@@ -4,17 +4,26 @@ import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch.actions import GroupAction
+from launch_ros.actions import PushRosNamespace
+
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    
+    robot_namespace = LaunchConfiguration('robot_namespace')
 
     bento_teleop = Node(
         package='bento_teleop',
         executable='teleop_node',
         name='teleop_node',
-        parameters=[ PathJoinSubstitution([ FindPackageShare('bento_teleop'), 'parameter', 'teleop.yaml' ]) ],
-        namespace=EnvironmentVariable( 'OPERATOR_NAMESPACE', default_value="box_opr" ),
+        parameters=[
+            {'robot_namespace': ["/", robot_namespace]},
+            PathJoinSubstitution([ FindPackageShare('bento_teleop'), 'parameter', 'teleop.yaml' ]),
+        ],
         output='screen',
         emulate_tty=True,
     )
@@ -26,10 +35,18 @@ def generate_launch_description():
             {'autorepeat_rate': 20.0},
             {'coalesce_interval_ms': 50}
         ],
-        namespace=EnvironmentVariable( 'OPERATOR_NAMESPACE', default_value="box_opr" ),
     )
 
     return LaunchDescription([
-        bento_teleop,
-        joystick,
+        DeclareLaunchArgument(
+            'robot_namespace',
+            default_value='bento',
+            description='set namespace for robot nodes'
+        ),
+        GroupAction(
+        actions=[
+            PushRosNamespace( [robot_namespace, "_opr"] ),
+            bento_teleop,
+            joystick,
+        ])
     ])
